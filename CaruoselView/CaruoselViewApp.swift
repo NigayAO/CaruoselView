50232e0f3fc51c57f4c2d7047a6f5afe10a221dd
Alik <Nigay_AO@bk.ru>
2022-10-17 19:08:38 +0600

    Initial Commit

---
+++ CaruoselView/CaruoselViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaruoselViewApp.swift
+//  CaruoselView
+//
+//  Created by Alik Nigay on 17.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct CaruoselViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
